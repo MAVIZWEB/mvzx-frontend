27eb53d417091d5c75059a549cb469e3501ddb15
MAZOLX <firmwealthinfo@gmail.com>
2025-08-19 17:42:12 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Leaderboard from "./pages/Leaderboard";
+import { BalanceProvider } from "./context/BalanceContext";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/leaderboard" element={<Leaderboard />} />
-    </Routes>
+    <BalanceProvider>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
+    </BalanceProvider>
   );
 }
