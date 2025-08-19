1bb202fa74a2c2014c018cfb4d5f5862c59ec6a0
MAZOLX <firmwealthinfo@gmail.com>
2025-08-19 17:20:14 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+ /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        cream: {
+          50: "#FFFDF5",
+          100: "#FFFDD0", // Soft cream for text & buttons
+          200: "#FAF3E0",
+        },
+        wine: {
+          800: "#6A0D2E", // Rich wine red
+          900: "#4B0E1A", // Darker wine base
+        },
+      },
+    },
+  },
   plugins: [],
 };
