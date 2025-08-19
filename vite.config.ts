e7b32a5cf67a0c6df4638914dfd9aa141e344121
MAZOLX <firmwealthinfo@gmail.com>
2025-08-19 05:08:56 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
- import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+ // vite.config.ts
 export default defineConfig({
-  plugins: [react()],
-  server: { host: true },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", // your backend port
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
