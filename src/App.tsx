3978876574ba1c0f28d565da25d6247756e5a26f
MAZOLX <firmwealthinfo@gmail.com>
2025-08-25 02:05:50 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
- import React from "react";
-import { Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import Buy from "./pages/Buy";
-import DirectBuy from "./pages/DirectBuy";
-import Airdrop from "./pages/Airdrop";
-import Mining from "./pages/Mining";
-import Escrow from "./pages/Escrow";
-import Voting from "./pages/Voting";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
-import Game from "./pages/Game";
-import Leaderboard from "./pages/Leaderboard";
-import AdminPanel from "./pages/AdminPanel";
 
-function App() {
+export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/buy" element={<Buy />} />
-      <Route path="/directbuy" element={<DirectBuy />} />
-      <Route path="/airdrop" element={<Airdrop />} />
-      <Route path="/mining" element={<Mining />} />
-      <Route path="/escrow" element={<Escrow />} />
-      <Route path="/voting" element={<Voting />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/game" element={<Game />} />
-      <Route path="/leaderboard" element={<Leaderboard />} />
-      <Route path="/admin" element={<AdminPanel />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
