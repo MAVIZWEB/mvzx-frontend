37b98077af3284bf37b59aace3fcce8718fe38ff
MAZOLX <firmwealthinfo@gmail.com>
2025-08-25 10:58:28 +0100

    Update LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
- import React, { useState, useEffect, useRef } from "react";
+  import React, { useState, useEffect, useRef } from "react";
 import { api } from "../services/api";
 import Wheel from "./Wheel";
 import BuyMVZX from "./BuyMVZX";
