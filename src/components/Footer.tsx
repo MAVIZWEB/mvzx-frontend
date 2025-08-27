d0f5457acccfdc418dc65270e7d9892bf990d7d6
MAVIZWEB <firmwealthinfo@gmail.com>
2025-08-27 16:07:29 +0100

    Delete src directory

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Footer: React.FC = () => (
-  <footer className="bg-gray-900 text-center p-4 mt-8">
-    <p>© 2025 MVZx. All rights reserved.</p>
-  </footer>
-);
