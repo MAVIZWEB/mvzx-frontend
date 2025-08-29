e56c8c81d9300f3123ab12e9d507aac91c972e86
MAVIZWEB <firmwealthinfo@gmail.com>
2025-08-29 22:40:30 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
- // frontend/src/App.tsx
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Purchase from "./pages/Purchase";
-import StakePage from "./pages/Stake";
+ import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Purchase from './pages/Purchase';
+import MatrixView from './pages/MatrixView';
+import Staking from './pages/Staking';
+import Withdraw from './pages/Withdraw';
 
-export default function App() {
+export default function App(){
   return (
-    <BrowserRouter>
-      <div className="p-4">
-        <nav className="mb-4">
-          <Link to="/" className="mr-4">Home</Link>
-          <Link to="/purchase" className="mr-4">Purchase</Link>
-          <Link to="/stake">Stake</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/purchase" element={<Purchase/>} />
-          <Route path="/stake" element={<StakePage/>} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+    <div className="min-h-screen bg-gray-50">
+      <Routes>
+        <Route path="/" element={<Signup/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/purchase" element={<Purchase/>} />
+        <Route path="/matrix" element={<MatrixView/>} />
+        <Route path="/staking" element={<Staking/>} />
+        <Route path="/withdraw" element={<Withdraw/>} />
+      </Routes>
+    </div>
+  )
 }
