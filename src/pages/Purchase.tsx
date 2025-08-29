f6834a41b23621cec30d3eb1a1000845ca06eeee
MAVIZWEB <firmwealthinfo@gmail.com>
2025-08-29 07:34:02 +0100

    Create Purchase.tsx

---
+++ src/pages/Purchase.tsx
@@ -0,0 +1,9 @@
+import PurchaseForm from '../components/PurchaseForm';
+export default function Purchase(){
+  return (
+    <div>
+      <h2>Buy MVZx</h2>
+      <PurchaseForm />
+    </div>
+  );
+}
