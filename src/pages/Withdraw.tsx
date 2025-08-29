e44bab39207635f7eca8239f50c2be8203d45a5a
MAVIZWEB <firmwealthinfo@gmail.com>
2025-08-29 08:30:54 +0100

    Create Withdraw.tsx

---
+++ src/pages/Withdraw.tsx
@@ -0,0 +1,9 @@
+import WithdrawForm from '../components/WithdrawForm';
+export default function Withdraw(){
+  return (
+    <div>
+      <h2>Withdraw</h2>
+      <WithdrawForm />
+    </div>
+  );
+}
