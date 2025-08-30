a033e8860abf8c8eb0784fbd69ca9ea6b0143c64
MAVIZWEB <firmwealthinfo@gmail.com>
2025-08-30 01:28:46 +0100

    Delete src directory

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
- export default function Footer(){
-  return (
-    <footer style={{padding:12, background:'#f3f4f6', color:'#111', textAlign:'center'}}>
-      © {new Date().getFullYear()} MVZx
-    </footer>
-  );
-}
--- src/components/Header.tsx
+++
@@ -1,7 +0,0 @@
- export default function Header(){
-  return (
-    <header style={{padding:12, background:'#1d4ed8', color:'#fff'}}>
-      <h1>MVZx Platform</h1>
-    </header>
-  );
-}
